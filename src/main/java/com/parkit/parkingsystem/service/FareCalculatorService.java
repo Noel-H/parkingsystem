554720480c6ff717577b9507aeaeb6eb98995e0a
Noel-H <noel.huberto@gmail.com>
2020-10-23 05:46:33 +0200

    Ajout de la méthode pour appliquer une réduction au utilisateurs récurrents

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,9 +1,17 @@
 package com.parkit.parkingsystem.service;
 
+import com.parkit.parkingsystem.config.DataBaseConfig;
+import com.parkit.parkingsystem.constants.DBConstants;
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class FareCalculatorService {
+
+    public DataBaseConfig dataBaseConfig = new DataBaseConfig();
 
     public void calculateFare(Ticket ticket){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
@@ -19,6 +27,7 @@ public class FareCalculatorService {
 
         if (durationInMinute<30) {
             ticket.setPrice(0);
+            //System.out.println("Gratuit en dessous des 30 premières minutes.");
         } else {
             switch (ticket.getParkingSpot().getParkingType()) {
                 case CAR: {
@@ -33,5 +42,42 @@ public class FareCalculatorService {
                     throw new IllegalArgumentException("Unkown Parking Type");
             }
         }
+
+        double percentOff = 5;
+        double actualPrice = ticket.getPrice();
+        //System.out.println(ticket.getVehicleRegNumber());
+        if (isAlreadyCame(ticket.getVehicleRegNumber())){
+            System.out.println("Prix avant réduction : " + actualPrice);
+            System.out.println("Une réduction de : " + (actualPrice*(percentOff/100)) + " est appliqué.");
+            ticket.setPrice((actualPrice-(actualPrice*(percentOff/100))));
+        }
+    }
+
+    private boolean isAlreadyCame(String registration) {
+        Boolean isGood = false;
+        Connection con = null;
+        try {
+            con = dataBaseConfig.getConnection();
+            PreparedStatement ps = con.prepareStatement(DBConstants.GET_REG);
+            ResultSet rs = ps.executeQuery();
+            while ((rs.next()) && !(isGood)) {
+                //System.out.println("Valeur de getString = " + rs.getString( "VEHICLE_REG_NUMBER"));
+                //System.out.println("La valeur de isGood = " + isGood);
+                 if ((registration.equals(rs.getString("VEHICLE_REG_NUMBER"))) && ((rs.getDate("OUT_TIME") != null ))){
+                     //System.out.println(rs.getString("VEHICLE_REG_NUMBER") + " --- " + rs.getDate("OUT_TIME"));
+                     //System.out.println("5% ok");
+                     isGood = true;
+                }
+            }
+            //System.out.println("boucle fini");
+            dataBaseConfig.closeResultSet(rs);
+            dataBaseConfig.closePreparedStatement(ps);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            dataBaseConfig.closeConnection(con);
+        }
+
+        return isGood;
     }
 }
